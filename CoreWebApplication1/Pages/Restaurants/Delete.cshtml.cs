08903b2cae3265b87c6e77036f5c9f779922b75a
fzb14 <fzb14@hotmail.com>
2020-09-04 17:12:57 +0800

    partial view

--- CoreWebApplication1/Pages/Restaurants/Delete.cshtml.cs
+++ CoreWebApplication1/Pages/Restaurants/Delete.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassLibrary1.Core;
 using ClassLibrary1.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,15 +13,30 @@ namespace CoreWebApplication1.Pages.Restaurants
     {
         private readonly IRestaurantData restaurantData;
 
+        public Restaurant Restaurant { get; set; }
         public DeleteModel(IRestaurantData restaurantData)
         {
             this.restaurantData = restaurantData;
         }
         public IActionResult OnGet(int id)
         {
-            var name = restaurantData.GetById(id).Name;
+            Restaurant = restaurantData.GetById(id);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            Restaurant = restaurantData.GetById(id);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
             restaurantData.Delete(id);
-            TempData["Message"] = name + " Delelted!";
+            TempData["Message"] = Restaurant.Name + " Delelted!";
             return RedirectToPage("./List");
         }
     }
